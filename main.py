afd1a3db7a931bb4bde20d0d2f42862985da3dd9
Elliot Kantor <elliotjkantor@gmail.com>
2021-04-20 08:19:29 -0400

    Added random color system

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import textwrap
 from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+
+def avg(t):
+    return sum(t) / len(t)
 
 LINE_LENGTH = 20
 # TODO: get random quote
-quote = "the only thing that matters in this life is butts"
+quote = "the only thing that matters in this life"
 formatted = "\n".join(textwrap.wrap(quote, LINE_LENGTH))
 
 # make image
@@ -11,15 +15,20 @@ image_width, image_height = (1000, 1000)
 fnt_size = 500
 text_width, text_height = 5000, 5000
 ratio = 0.85
+bg_color = tuple(np.random.choice(range(256), size=3))
+fg_color = 'white' if avg(bg_color) < 128 else 'black'
+
 while text_width > ratio * image_width or text_height > ratio * image_height:
-    img = Image.new('RGB', (image_width, image_height), color=(50,50,50))
+
+    img = Image.new('RGB', (image_width, image_height), color=bg_color)
     fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', fnt_size)
     d = ImageDraw.Draw(img)
+
     text_width, text_height = d.textsize(formatted, font=fnt)
     fnt_size -= 5
 
 # center text
 x_pos = int((image_width - text_width) / 2)
 y_pos = int((image_height - text_height) / 2)
-d.text((x_pos, y_pos), formatted, font=fnt, fill=(255, 255, 0))
+d.text((x_pos, y_pos), formatted, font=fnt, fill=fg_color)
 img.save('img.png')
